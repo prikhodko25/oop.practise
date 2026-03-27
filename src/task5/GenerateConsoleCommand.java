2ece36da96788bbcb03b8b67d1c569476f8ac133
Приходько Софія <sofiykapiryet@gmail.com>
2026-03-27 11:33:22 +0200

    Create GenerateConsoleCommand.java

---
+++ src/task5/GenerateConsoleCommand.java
@@ -0,0 +1,49 @@
+package ex17;
+
+/**
+ * Консольна команда Generate
+ * Шаблон Command
+ * 
+ * @author Student
+ * @version 1.0
+ */
+public class GenerateConsoleCommand implements ConsoleCommand {
+    
+    /** Об'єкт для відображення результатів */
+    private View view;
+    
+    /**
+     * Ініціалізує команду
+     * @param view об'єкт View
+     */
+    public GenerateConsoleCommand(View view) {
+        this.view = view;
+    }
+    
+    @Override
+    public char getKey() {
+        return 'g';
+    }
+    
+    @Override
+    public String getDescription() {
+        return "генерувати випадкові дані";
+    }
+    
+    @Override
+    public String toString() {
+        return "'g' - " + getDescription();
+    }
+    
+    @Override
+    public void execute() {
+        System.out.println("Генерація випадкових даних...");
+        view.viewInit();
+        view.viewShow();
+    }
+    
+    @Override
+    public void undo() {
+        System.out.println("Неможливо скасувати генерацію.");
+    }
+}
