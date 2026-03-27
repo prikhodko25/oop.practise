189f9045cd4ae15a8cfce6bf6db241c8edbe1fe8
Приходько Софія <sofiykapiryet@gmail.com>
2026-03-27 11:37:40 +0200

    Create Main.java

---
+++ src/task5/Main.java
@@ -0,0 +1,19 @@
+package ex17;
+
+/**
+ * Головний клас програми
+ * 
+ * @author Student
+ * @version 4.0
+ */
+public class Main {
+    
+    /**
+     * Точка входу в програму
+     * @param args параметри запуску
+     */
+    public static void main(String[] args) {
+        Application app = Application.getInstance();
+        app.run();
+    }
+}
